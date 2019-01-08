ee7b2b233639db8f2000261b79ea855669491446
GerVincent <32233983+GerVincent@users.noreply.github.com>
2019-01-08 10:32:18 -0500

    Inserta Datos para Login
    Inserta Datos para Login

--- Sql_Insert_Empresas.sql
+++ Sql_Insert_Empresas.sql
@@ -1,1 +1,6 @@
-INSERT INTO empresas (Nombre_RZ) VALUES ("LECHE GLORIA");
+INSERT INTO empresas (empresas.Nombre_RZ) VALUES ("LECHE GLORIA"); 
+INSERT INTO usuarios (usuarios.Nombre,usuarios.Email,usuarios.Password) VALUES ("GERBER","gerber.risco@gmail.com","123"); 
+INSERT INTO empresas_usuarios (empresas_usuarios.Empresas_Id,empresas_usuarios.Usuarios_Id) VALUES (1,1);
+SELECT * FROM empresas; 
+SELECT * FROM usuarios;
+SELECT * FROM empresas_usuarios;
